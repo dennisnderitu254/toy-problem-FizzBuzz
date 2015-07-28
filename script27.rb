370a9848dbc2c0ce4add26222b3994f9fc3603d6
Dennis Nderitu <dknderitu@gmail.com>
2015-07-28 10:04:14 +0300

    My greatest invention yet

---
+++ script27.rb
@@ -0,0 +1,18 @@
+#FIZZBUZZ toy problem
+
+for num in 1...100
+  puts num
+end
+
+num = gets.chomp
+
+if num % 3 and 5 == 0
+	puts "FizzBuzz"
+elsif num % 3 == 0
+	puts "Fizz"
+elsif num % 5 == 0
+	puts "Buzz"
+else
+	puts num 
+end
+
